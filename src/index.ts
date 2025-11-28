4067eb5c101745f6b3e5a6b0d1c9649390055cf1
sergeyprokofiev <sp@prosoftsolutions.cloud>
2025-11-28 15:22:18 +0100

    main parameters are specified

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,34 @@
 import * as cdk from "aws-cdk-lib";
 import { PromoDeployStack } from "./stack";
 
-export function Deploy(): cdk.App {
+export interface DeployProps {
+  region: string;
+  awsAccount: string;
+  cdkBucket: string;
+}
+
+export function Deploy(props: DeployProps): cdk.App {
+  const { region, awsAccount, cdkBucket } = props;
+
   const app = new cdk.App();
 
   new PromoDeployStack(app, "PromoDeployStack", {
     env: {
-      account: process.env.CDK_DEFAULT_ACCOUNT,
-      region: "us-east-1",
+      account: awsAccount,
+      region: region,
     },
+    synthesizer: new cdk.DefaultStackSynthesizer({
+      fileAssetsBucketName: cdkBucket,
+    }),
   });
 
   return app;
 }
 
 // Run deployment when executed directly
-const app = Deploy();
+const app = Deploy({
+  region: process.env.CDK_DEFAULT_REGION || "us-east-1",
+  awsAccount: process.env.CDK_DEFAULT_ACCOUNT || "",
+  cdkBucket: process.env.CDK_BUCKET || "",
+});
 app.synth();
